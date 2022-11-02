78045fc50143b0aef255c77c2189efb722e6233b
Wong Wei Jian <weijian9091@gmail.com>
2022-11-02 15:04:48 +0800

    add the button that allows users to specify the size of the grid

--- myScript.js
+++ myScript.js
@@ -1,20 +1,44 @@
-// create 16X16 grid of divs using javascipt
-let container = document.createElement('div');
-
-for(let i=0;i<16;i++){
-
-    for(let j=0;j<16;j++){
-        let div = document.createElement('div');
-        div.style.display = 'inline-block';
-        div.textContent ='.';
-        container.appendChild(div);
+// create n x n grid of divs using javascipt
 
 
+function createGrid(n){
+    let container = document.createElement('div');
+    container.classList.add('grid');
+    if (n > 100){
+        alert('Please choose a number below 100');
+        return;
     }
-    let lineBreak = document.createElement('br');
-    container.appendChild(lineBreak);
+    for(let i=0;i<n;i++){
+
+        for(let j=0;j<n;j++){
+            let div = document.createElement('div');
+            div.style.display = 'inline-block';
+            div.textContent='.'
+            container.appendChild(div);
+            div.addEventListener('mouseover', function () {
+                div.style.backgroundColor = 'black';
+            })
+        }
+        let lineBreak = document.createElement('br');
+        container.appendChild(lineBreak);
+    
+    }
+    
+    document.body.appendChild(container);
 
 }
 
-document.body.appendChild(container);
-console.log('hello')
+let number;
+
+// reference the button and use it to change grid
+let button = document.querySelector('.button');
+button.addEventListener('click', () => {
+    // Check if the document already has a grid, if it does remove it
+    if (document.body.contains(document.querySelector('.grid'))) {
+        document.body.removeChild(document.querySelector('.grid'));
+    };
+    number = prompt('Choose a value of n. This will create a grid of n x n size');
+    createGrid(number);
+})
+
+
